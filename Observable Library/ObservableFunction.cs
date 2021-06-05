dd172d2ad8524c123e712110e9d712fa3de5b369
Flynn <me+github@flynn1179.net>
2021-06-05 19:32:09 +0100

    Add 2 input function to the ObservableFunction class.

--- Observable Library/ObservableFunction.cs
+++ Observable Library/ObservableFunction.cs
@@ -48,4 +48,59 @@ namespace Flynn1179.Observable
             private set => this.Set(ref this.result, value);
         }
     }
+
+    /// <summary>
+    /// Represents a function with an observable result, that updates whenever a change to the input causes a change.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first function input.</typeparam>
+    /// <typeparam name="T2">The type of the second function input.</typeparam>
+    /// <typeparam name="TResult">The result type of the function.</typeparam>
+    public class ObservableFunction<T1, T2, TResult> : ObservableObject
+    {
+        private readonly Func<T1, T2, TResult> func;
+
+        private TResult result;
+
+        private T1 input1;
+
+        private T2 input2;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObservableFunction{T1, T2, TResult}"/> class.
+        /// </summary>
+        /// <param name="func">The function to be applied to the input.</param>
+        public ObservableFunction(Func<T1, T2, TResult> func)
+        {
+            func.ThrowIfNull(nameof(func));
+            this.func = func;
+            this.result = this.func(default, default);
+        }
+
+        /// <summary>
+        /// Gets or sets the first input for the function.
+        /// </summary>
+        public T1 Input1
+        {
+            get => this.input1;
+            set => this.Set(ref this.input1, value, value => this.Result = this.func(value, this.Input2));
+        }
+
+        /// <summary>
+        /// Gets or sets the second input for the function.
+        /// </summary>
+        public T2 Input2
+        {
+            get => this.input2;
+            set => this.Set(ref this.input2, value, value => this.Result = this.func(this.Input1, value));
+        }
+
+        /// <summary>
+        /// Gets the result of the function.
+        /// </summary>
+        public TResult Result
+        {
+            get => this.result;
+            private set => this.Set(ref this.result, value);
+        }
+    }
 }
